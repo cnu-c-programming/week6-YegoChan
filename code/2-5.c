34e8fefd927f0362bc50609a4f9c1b786f0bafd6
unknown <YegoChan>
2026-04-10 13:04:29 +0900

    Update 2-5.c

---
+++ code/2-5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int arr[6] = {1, 2, 3, 4, 5, 6};
+    int* p = arr;
+    int** pp = &p;
+
+    for (int i = 0; i < 6; i++) {
+        printf("%d ", *(*pp + i));
+        // = *(p + i)
+        // = *(arr + i)
+    }
+
+    return 0;
+}
