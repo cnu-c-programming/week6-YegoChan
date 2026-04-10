05f390ae5f2df8e033702e2db1b96890013fdb65
unknown <YegoChan>
2026-04-10 12:43:52 +0900

    Update 2-3.c

--- code/2-3.c
+++ code/2-3.c
@@ -4,6 +4,7 @@ void print_2d_arr(int* element, int row_size, int col_size) {
     for (int i = 0; i < row_size; i++) {
         for (int j = 0; j < col_size; j++) {
             printf("%d ", element[i*col_size + j]);
+            // 배열은 메모리에서 딱 붙은 일렬로 나열되니까, 이런 식의 접근이 가능
         }
         printf("\n");
     }
@@ -13,4 +14,5 @@ int main() {
     int arr[2][3] = {1, 2, 3, 4, 5, 6};
 
     print_2d_arr(&arr[0][0], 2, 3);
+    // arr[0][0], 0행 0열 원소의 주소 전달
 }
