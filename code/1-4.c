aef76d034fe6380ec27988f22037b3dc9f79166b
unknown <YegoChan>
2026-04-10 12:16:16 +0900

    Update 1-4.c

--- code/1-4.c
+++ code/1-4.c
@@ -10,8 +10,11 @@ int main() {
     printf("%zu\n", sizeof(**p));  // int[4]
     printf("%zu\n", sizeof(***p)); // int
 
-    printf("%p\n", (void*)p);
-    printf("%p\n", (void*)*p);
-    printf("%p\n", (void*)**p);
+    printf("%p\n", (void*)p);   // int(*)[3][4], 근데 그게 &arr[0]
+    printf("%p\n", (void*)*p);  // int(*)[4], 근데 그게 &arr[0][0]
+    printf("%p\n", (void*)**p); // int, 근데 그게 &arr[0][0][0]
+                                // 주소가 같다고 타입이 같진 않더라
     printf("%d\n", ***p);
 }
+
+
